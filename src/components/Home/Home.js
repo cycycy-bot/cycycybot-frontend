8d70fc55afb903fae13faa05d5ff6b13822e7f5b
Cyryll Galon <galoncyryll@gmail.com>
2019-07-23 16:12:47 -0700

    add home design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import './Home.css';
+
 const Home = () => (
-  <div>
-    <h1>HOME</h1>
+  <div className="home-container">
+    <div className="home-contents">
+      <div className="home-main">
+        <h1>cycycy bot is a simple moderating/meme bot for Discord Servers!</h1>
+        <a href="https://discordapp.com/oauth2/authorize?client_id=530305194131456000&scope=bot&permissions=8" className="login-button">
+          Add to Discord
+        </a>
+      </div>
+    </div>
   </div>
 );
 
